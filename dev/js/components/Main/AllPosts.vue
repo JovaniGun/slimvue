a7f57cfc24c34884eceba7774f4bcf2abd7b6af5
Vladik <yapichoy@gmail.com>
2019-05-09 16:48:06 +0300

    l

--- dev/js/components/Main/AllPosts.vue
+++ dev/js/components/Main/AllPosts.vue
@@ -1,13 +1,13 @@
 <template>
 <div class="container">
-  <div class="row justify-content-center mt-4 pt-5">
+  <div class="row justify-content-center mt-4 pt-2">
       <div class="card m-3" style="width: 18rem;"
         v-for="post in posts" :key="post.id"
       >
         <div class="card-body">
           <div class="card-header">
             <h5 class="card-title">{{ post.title }}</h5>
-            <button @click.prevent="deletePostById(post.id)"><i class="fas fa-trash" style="margin-bottom: .75rem; color:red;"></i></button>
+            <button @click.prevent="deletePostById(post.id)"><i class="fas fa-times" style="font-size: 1.2rem; color:red;"></i></button>
           </div>
           <router-link class="card-text" :to="`/post/${post.id}`">{{ post.preview }}</router-link>
           <!-- <a href="#" class="card-link">Ссылка карты</a> -->
@@ -19,11 +19,17 @@
 <style scoped>
   .card-header{
     margin: 0;
+    margin-bottom: 2.3rem;
     padding: 0;
     display: flex;
     justify-content: space-between;
-    align-items: center;
+    align-items: flex-start;
     background-color: #fff; 
+  }
+   a.card-text{
+    margin-top: 1.2rem;
+    font-size: 1.1rem;
+    text-decoration: none;
   }
   button{
     background-color: #fff;
